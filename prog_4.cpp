6c9963df694520d4b47a89d48db5a0d70de3ae07
KUNALLATTA29 <95677039+KUNALLATTA29@users.noreply.github.com>
2024-06-17 15:37:27 +0530

    Update prog_4.cpp

--- prog_4.cpp
+++ prog_4.cpp
@@ -1,36 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+
 int main(){
-    map<string,int>arr{
-        {"kunal",2200},
-        {"babu",3300},
-        {"lala",5500},
-        {"jagu",3455},
-        {"damru",6600}
+    
+    map<string,int> arr{
+        {"kunal",3300},
+        {"babu",2200},
+        {"raju",4500},
+        {"jagu",8800},
+        {"kaliya",2700},
+        {"lali",8800}
     };
-    int sal;
-    vector<int> arr2;
-    for(int i=0;i<3;i++){
-        cin>>sal;
-        arr2.push_back(sal);
-    }
-    vector<string> rem;
-    for(auto it:arr2){
-        for(auto &k:arr){
-            if(k.second==it){
-                rem.push_back(k.first);
-            }
+    
+    int s;
+    cin>>s;
+    vector<string> key;
+    for(auto it:arr){
+        if(it.second==s){
+            key.push_back(it.first);
         }
     }
-    
-    for(auto &it:rem){
+    for(auto it:key){
         arr.erase(it);
     }
     for(auto it:arr){
-        cout<<it.first<<":"<<it.second<<endl;
+        cout<<it.first<<"="<<it.second<<endl;
     }
-    
-    
-    
-    return 0;
 }
